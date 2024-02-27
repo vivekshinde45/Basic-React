a40d5f23ffb9dc80a02f38a80438c4f67bc18690
Vivek Shinde <vivshind@publicisgroupe.net>
2024-02-27 09:55:34 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/*
+
+    <div id="parent">
+        <div id="child">
+            <h1>I am H1 tag</h1>
+        </div>
+    </div>
+
+*/
+
+const parent = React.createElement("div", 
+    {id:"parent"},
+    [React.createElement("div", {id:"child1"},
+        [React.createElement("h1", {}, "I am H1 tag"),
+        React.createElement("h2", {}, "I am H2 tag")]),
+    React.createElement("div", {id:"child2"},
+        [React.createElement("h1", {}, "I am H1 tag"),
+        React.createElement("h2", {}, "I am H2 tag")])
+    ]
+);
+
+// const heading = React.createElement("h1", {}, "Hello world from React!");
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
