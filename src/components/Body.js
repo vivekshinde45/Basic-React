54cf453145e46c4fbfa04602c9e10a56203db672
Vivek Shinde <vivshind@publicisgroupe.net>
2024-03-05 15:39:47 +0530

    hooks (useStates) added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,37 @@
-import RestaurantCard from '../components/RestaurantCard'
-import { resList } from '../utils/constants';
-
+import { useState } from "react";
+import RestaurantCard from "../components/RestaurantCard";
+import { resList } from "../utils/constants";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-        <div className="body">
-            <div className="search-bar">Search</div>
-            <div className="res-container">
-                {resList.map((restaurant) => {
-                    return (
-                        <RestaurantCard key={restaurant.data.id} resData={restaurant} />
-                    );
-                })};
-            </div>
-        </div>
-    );
-}
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            console.log(filteredList);
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          );
+        })}
+        ;
+      </div>
+    </div>
+  );
+};
 
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,29 +1,33 @@
 import { IMG_CDN_URL, resList } from "../utils/constants";
 
 const RestaurantCard = (props) => {
-    const { resData } = props;
+  const { resData } = props;
 
-    const {
-        cloudinaryImageId,
-        name,
-        cuisines,
-        area,
-        lastMileTravelString,
-        costForTwoString,
-        avgRating,
-    } = resData?.data;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    area,
+    lastMileTravelString,
+    costForTwoString,
+    avgRating,
+  } = resData?.data;
 
-    return (
-        <div className="res-card">
-            <img className="dish-img" alt="biryani-image" src={IMG_CDN_URL + cloudinaryImageId}/>
-            {/* {console.log(cloudinaryImageId)} */}
-            <h2>{name}</h2>
-            <h3>{cuisines.join(", ")}</h3>
-            <h3>{avgRating} Stars</h3>
-            <h4>{lastMileTravelString} away</h4>
-            <h4>{costForTwoString}</h4>
-        </div>
-    );
-}
+  return (
+    <div className="res-card">
+      <img
+        className="dish-img"
+        alt="biryani-image"
+        src={IMG_CDN_URL + cloudinaryImageId}
+      />
+      {/* {console.log(cloudinaryImageId)} */}
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h3>{avgRating} Stars</h3>
+      <h4>{lastMileTravelString} away</h4>
+      <h4>{costForTwoString}</h4>
+    </div>
+  );
+};
 
 export default RestaurantCard;
